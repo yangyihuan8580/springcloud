8088878df95d33e921ad6827d6efbcd7f2555656
yangyihuan <952010431@qq.com>
2019-11-18 14:48:35 +0800

    master 版本提交

---
+++ park-service/park-provider/src/main/java/com/yyh/park/service/impl/ParkServiceImpl.java
@@ -0,0 +1,32 @@
+package com.yyh.park.service.impl;
+
+import com.yyh.common.base.ABaseServiceImpl;
+import com.yyh.common.base.IBaseDao;
+import com.yyh.park.dao.ParkDao;
+import com.yyh.park.entity.Park;
+import com.yyh.park.service.ParkService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * 
+ * <br>
+ * <b>功能：</b>ParkService<br>
+ */
+@Service("parkService")
+public class ParkServiceImpl extends ABaseServiceImpl<Park> implements ParkService {
+  private final static Logger log= LoggerFactory.getLogger(ParkServiceImpl.class);
+	
+
+	@Autowired
+    private ParkDao parkDao;
+	
+
+	@Override
+	protected IBaseDao<Park> getBaseDao() {
+		return parkDao;
+	}
+
+}
