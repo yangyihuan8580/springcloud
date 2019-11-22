868a072ebfcf0aaf1a9a0be34783d2765a9bf691
yangyihuan <952010431@qq.com>
2019-11-22 17:57:43 +0800

    master 版本提交

---
+++ cloud-feign/cloud-feign-park/src/main/java/com/yyh/park/feign/ParkFeignClient.java
@@ -0,0 +1,20 @@
+package com.yyh.park.feign;
+
+import com.yyh.common.base.Result;
+import com.yyh.park.dto.ParkDTO;
+import com.yyh.park.feign.fallback.ParkFallback;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@FeignClient(value = "park-provider", fallback = ParkFallback.class)
+public interface ParkFeignClient {
+
+    @RequestMapping(value = "/park", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE,  produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    Result<ParkDTO> add(ParkDTO parkDTO);
+
+    @RequestMapping(value = "/park/{parkId}", method = RequestMethod.GET, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    Result<ParkDTO> selectParkById(@PathVariable("parkId") Long parkId);
+}
