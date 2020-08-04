dfe92807c4c0863c0d2596614dc3a62a1b0c0ba9
yangyihuan8580 <952010431@qq.com>
2020-08-04 14:09:54 +0800

    xuhui1.0 项目框架搭建

---
+++ cloud-web/cloud-web-cps/src/main/java/wechat/CloudWebCpsApplication.java
@@ -0,0 +1,13 @@
+package wechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudWebCpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudWebCpsApplication.class, args);
+	}
+
+}
---
+++ cloud-web/cloud-web-cps/src/test/java/wechat/CloudWebCpsApplicationTests.java
@@ -0,0 +1,13 @@
+package wechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudWebCpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
